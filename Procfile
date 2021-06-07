e794a06be7f5fd1e14995ca911f8a8e80d7a8cc2
Яргин Юрий <yargin.y@gmail.com>
2021-06-07 13:36:52 +0300

    asd

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh target/bin/webapp
+sh target/bin/webapp
--- src/main/java/com/example/testjsp/TestJspApplication.java
+++ src/main/java/com/example/testjsp/TestJspApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestJspApplication {
-
+//    com.example.testjsp.TestJspApplication
     public static void main(String[] args) {
         SpringApplication.run(TestJspApplication.class, args);
     }
