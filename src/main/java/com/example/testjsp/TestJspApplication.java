9ae36d8447f1c3d560d79126d54956dae493b2b0
Яргин Юрий <yargin.y@gmail.com>
2021-06-07 14:33:43 +0300

    asd

--- src/main/java/com/example/testjsp/TestJspApplication.java
+++ src/main/java/com/example/testjsp/TestJspApplication.java
@@ -2,10 +2,17 @@ package com.example.testjsp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class TestJspApplication {
 //    com.example.testjsp.TestJspApplication
+
+    public TestJspApplication(Environment environment) {
+        System.out.println(environment.getProperty("========================================================================="));
+        System.out.println(environment.getProperty("PORT"));
+        System.out.println(environment.getProperty("========================================================================="));
+    }
     public static void main(String[] args) {
         SpringApplication.run(TestJspApplication.class, args);
     }
